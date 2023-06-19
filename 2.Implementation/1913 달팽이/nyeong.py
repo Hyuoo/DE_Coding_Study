cddb6ac0f4e1c8c886ce443b180cf83fb7d45887
An Nyeong <me@annyeong.me>
2023-06-19 21:30:03 +0900

    improve: 달팽이 주석 추가

--- 2.Implementation/1913 달팽이/nyeong.py
+++ 2.Implementation/1913 달팽이/nyeong.py
@@ -11,16 +11,24 @@ class Direction(Enum):
         return self.name
 
     def clockwise(self) -> Self:
-        if self.name == 'UP':
+        """
+        Example:
+            >>> dir = Direction.UP
+            >>> assert Direction.RIGHT == dir.clockwise()
+        """
+        if self == self.UP:
             return self.RIGHT
-        elif self.name == 'RIGHT':
+        elif self == self.RIGHT:
             return self.DOWN
-        elif self.name == 'DOWN':
+        elif self == self.DOWN:
             return self.LEFT
-        elif self.name == 'LEFT':
+        elif self == self.LEFT:
             return self.UP
 
 def next(x: int, y: int, direction: Direction) -> Tuple[int, int]:
+    """
+    x, y가 주어졌을 때 direction 방향으로 이동할 경우의 좌표를 반환합니다.
+    """
     if direction == Direction.UP:
         return (x, y - 1)
     elif direction == Direction.DOWN:
@@ -37,16 +45,22 @@ class Snail():
     def __init__(self, n: int, target_number: int):
         self.ns = [[0 for _ in range(0, n)] for _ in range(0, n)]
 
+        # 시작점 잡기
         x = y = n // 2
-        # current_level: means the root of the target number of the inner
-        # cycle square.
-        # TODO
+
+        # current_level은 1, 3, 5, 7, ...와 같은 홀수입니다.
         current_level = 1
+        # stack에 방향을 저장합니다.
         stack = []
         for i in range(1, n * n + 1):
             self.ns[y][x] = i
             if i == target_number:
                 self.target_number_position = (x, y)
+
+            # 홀수의 제곱을 만나면 현재 단계가 끝난 것입니다.
+            # 다음 목표를 설정하고 방향을 무식하게 스택에 때려 박습니다
+            # 예:
+            #     n = 3일 경우 9면 끝남
             if i == current_level ** 2:
                 current_level += 2
                 stack += [Direction.UP] * (current_level - 1)
