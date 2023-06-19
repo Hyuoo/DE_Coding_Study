09e479216999acbd8052d5334fcfbfec2713fb56
Hyungwoo Lim <43870227+Hyuoo@users.noreply.github.com>
2023-06-19 11:31:36 +0900

    988ms

---
+++ 2.Implementation/1913 달팽이/hyuoo.py
@@ -0,0 +1,31 @@
+n=int(input())
+f=int(input())
+a=[[0 for _ in range(n)] for _ in range(n)]
+d=[0,0,0,0]
+m=1
+i=j=n//2
+while m<=n**2:
+    a[i][j]=m
+    if m==f:
+        f=[i+1,j+1]
+    if d[0]:
+        d[0]-=1
+        j+=1
+    elif d[1]:
+        d[1]-=1
+        i+=1
+    elif d[2]:
+        d[2]-=1
+        j-=1
+    elif d[3]:
+        d[3]-=1
+        i-=1
+    else:
+        t=int(m**(1/2))
+        d = [t,t+1,t+1,t+1]
+        i-=1
+    m+=1
+
+for i in a:
+    print(*i)
+print(*f)
